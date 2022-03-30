82b43e0b3a209d58215046bd9d6cca6b4c6d689f
wi20023 <89901288+wi20023@users.noreply.github.com>
2022-03-30 21:28:34 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -103,7 +103,7 @@ app.post('/client_post', (req, res) => {
 
 // ###################### DATABASE PART (user) ######################
 // GET path for database
-app.get('/database', (req, res) => {
+app.get('/database_user/user', (req, res) => {
     console.log("Request to load all entries from user");
     // Prepare the get query
     connection.query("SELECT * FROM `user`;", function (error, results, fields) {
@@ -120,8 +120,8 @@ app.get('/database', (req, res) => {
     });
 });
 
-// DELETE path for database
-app.delete('/database/:id', (req, res) => {
+// DELETE path for database -> not used
+app.delete('/database_user/user/:id', (req, res) => {
     // This path will delete an entry. For example the path would look like DELETE '/database/5' -> This will delete number 5
     let id = req.params.id; // <- load the ID from the path
     console.log("Request to delete Item: " + id); // <- log for debugging
@@ -146,7 +146,7 @@ app.delete('/database/:id', (req, res) => {
 });
 
 // POST path for database
-app.post('/database', (req, res) => {
+app.post('/database_user/user', (req, res) => {
     // This will add a new row. So we're getting a JSON from the webbrowser which needs to be checked for correctness and later
     // it will be added to the database with a query.
     if (typeof req.body !== "undefined" && typeof req.body.title !== "undefined" && typeof req.body.quantity !== "undefined" && typeof req.body.unit !== "undefined") {
@@ -159,7 +159,12 @@ app.post('/database', (req, res) => {
     
         // Actual executing the query. Please keep in mind that this is for learning and education.
         // In real production environment, this has to be secure for SQL injection!
-        connection.query("INSERT INTO `user` (`id`, `user`, `password`) VALUES (NULL, '" + user + "', '" + password + "', current_date());", function (error, results, fields) {
+        connection.query("INSERT INTO `user` (`id`, `user`, `password`, `created_at`) VALUES (NULL, '" + user + "', '" + password + "', current_date());", function (error, results, fields) {
+                // SQL-Injection vermeiden:  
+        // connection.query("INSERT INTO 'user' (`id`, `user`, `password`, `created_at`) VALUES (NULL, '" + ? + "', '" + ? + "', current_date());") , [
+        // req.body.user,
+        // req.body.password
+        //], function (error, results, fields) {       
             if (error) {
                 // we got an errror - inform the client
                 console.error(error); // <- log error in server
@@ -182,12 +187,12 @@ app.post('/database', (req, res) => {
 // ###################### DATABASE PART END (user) ######################
 
 
-// ###################### DATABASE PART (main list) ######################
+// ###################### DATABASE PART (mainList) ######################
 // GET path for database
-app.get('/database', (req, res) => {
-    console.log("Request to load all entries from main list");
+app.get('/database/mainList', (req, res) => {
+    console.log("Request to load all entries from mainList");
     // Prepare the get query
-    connection.query("SELECT * FROM `main list`;", function (error, results, fields) {
+    connection.query("SELECT * FROM `mainList`;", function (error, results, fields) {
         if (error) {
             // we got an errror - inform the client
             console.error(error); // <- log error in server
@@ -202,17 +207,18 @@ app.get('/database', (req, res) => {
 });
 
 // DELETE path for database
-app.delete('/database/:id', (req, res) => {
+app.delete('/database/mainList/:id', (req, res) => {
     // This path will delete an entry. For example the path would look like DELETE '/database/5' -> This will delete number 5
     let id = req.params.id; // <- load the ID from the path
     console.log("Request to delete Item: " + id); // <- log for debugging
 
     // Actual executing the query to delete it from the server
     // Please keep in mind to secure this for SQL injection!
-    connection.query("DELETE FROM `main list` WHERE `main list`.`id` = " + id + ";", function (error, results, fields) {
+    // connection.query("DELETE FROM `mainList` WHERE `mainList`.`id` = " + id + ";", function (error, results, fields) {
     // SQL-Injection vermeiden:  
-    // const data = request.body;
-    // connection.query("DELETE FROM `main list` WHERE `id` = ?", [data.id], function (error, results, fields) {  
+     connection.query("DELETE FROM `mainList` WHERE `mainList`.`id` = ?", [
+     req.body.id
+    ], function (error, results, fields) {  
         if (error) {
             // we got an errror - inform the client
             console.error(error); // <- log error in server
@@ -227,7 +233,7 @@ app.delete('/database/:id', (req, res) => {
 });
 
 // POST path for database
-app.post('/database', (req, res) => {
+app.post('/database/mainList', (req, res) => {
     // This will add a new row. So we're getting a JSON from the webbrowser which needs to be checked for correctness and later
     // it will be added to the database with a query.
     if (typeof req.body !== "undefined" && typeof req.body.title !== "undefined" && typeof req.body.quantity !== "undefined" && typeof req.body.unit !== "undefined") {
@@ -240,7 +246,13 @@ app.post('/database', (req, res) => {
     
         // Actual executing the query. Please keep in mind that this is for learning and education.
         // In real production environment, this has to be secure for SQL injection!
-        connection.query("INSERT INTO `main list` (`id`, `title`, `quantity`, `unit`, `created_at`) VALUES (NULL, '" + title + "', '" + quantity + "', '" + unit + "', current_date());", function (error, results, fields) {
+        connection.query("INSERT INTO `mainList` (`id`, `title`, `quantity`, `unit`, `created_at`) VALUES (NULL, '" + title + "', '" + quantity + "', '" + unit + "', current_date());", function (error, results, fields) {
+        // SQL-Injection vermeiden:  
+        // connection.query("INSERT INTO 'list3' (`id`, `title`, `quantity`, `unit`, `created_at`) VALUES (NULL, '" + ? + "', '" + ? + "', '" + ? + "', current_date());") , [
+        // req.body.title,
+        // req.body.quantity,
+        // req.body.unit
+        //], function (error, results, fields) {       
             if (error) {
                 // we got an errror - inform the client
                 console.error(error); // <- log error in server
@@ -260,13 +272,13 @@ app.post('/database', (req, res) => {
         res.status(400).json({ message: 'This function requries a body with "title", "quantity" and "unit"' });
     }
 });
-// ###################### DATABASE PART END (Main list) ######################
+// ###################### DATABASE PART END (mainList) ######################
 
 
 // ###################### DATABASE PART (list2) ######################
 // GET path for database
 app.get('/database/list2', (req, res) => {
-    console.log("Request to load all entries from main list");
+    console.log("Request to load all entries from mainList");
     // Prepare the get query
     connection.query("SELECT * FROM `list2`;", function (error, results, fields) {
         if (error) {
@@ -292,8 +304,9 @@ app.delete('/database/list2/:id', (req, res) => {
     // Please keep in mind to secure this for SQL injection!
     connection.query("DELETE FROM `list2` WHERE `list2`.`id` = " + id + ";", function (error, results, fields) {
     // SQL-Injection vermeiden:  
-    // const data = request.body;
-    // connection.query("DELETE FROM `main list` WHERE `id` = ?", [data.id], function (error, results, fields) {  
+    // connection.query("DELETE FROM `list2` WHERE `id` = ?", [
+    // req.body.id
+    //], function (error, results, fields) {  
         if (error) {
             // we got an errror - inform the client
             console.error(error); // <- log error in server
@@ -322,6 +335,12 @@ app.post('/database/list2', (req, res) => {
         // Actual executing the query. Please keep in mind that this is for learning and education.
         // In real production environment, this has to be secure for SQL injection!
         connection.query("INSERT INTO `list2` (`id`, `title`, `quantity`, `unit`, `created_at`) VALUES (NULL, '" + title + "', '" + quantity + "', '" + unit + "', current_date());", function (error, results, fields) {
+        // SQL-Injection vermeiden:  
+        // connection.query("INSERT INTO 'list2' (`id`, `title`, `quantity`, `unit`, `created_at`) VALUES (NULL, '" + ? + "', '" + ? + "', '" + ? + "', current_date());") , [
+        // req.body.title,
+        // req.body.quantity,
+        // req.body.unit
+        //], function (error, results, fields) {       
             if (error) {
                 // we got an errror - inform the client
                 console.error(error); // <- log error in server
@@ -346,7 +365,7 @@ app.post('/database/list2', (req, res) => {
 // ###################### DATABASE PART (list3) ######################
 // GET path for database
 app.get('/database/list3', (req, res) => {
-    console.log("Request to load all entries from main list");
+    console.log("Request to load all entries from mainList");
     // Prepare the get query
     connection.query("SELECT * FROM `list3`;", function (error, results, fields) {
         if (error) {
@@ -371,10 +390,10 @@ app.delete('/database/list3/:id', (req, res) => {
     // Actual executing the query to delete it from the server
     // Please keep in mind to secure this for SQL injection!
     connection.query("DELETE FROM `list3` WHERE `list3`.`id` = " + id + ";", function (error, results, fields) {
-    
     // SQL-Injection vermeiden:  
-    // const data = request.body;
-    // connection.query("DELETE FROM `main list` WHERE `id` = ?", [data.id], function (error, results, fields) {  
+    // connection.query("DELETE FROM `list3` WHERE `id` = ?", [
+    // req.body.id
+    //], function (error, results, fields) { 
         if (error) {
             // we got an errror - inform the client
             console.error(error); // <- log error in server
@@ -403,6 +422,12 @@ app.post('/database/list3', (req, res) => {
         // Actual executing the query. Please keep in mind that this is for learning and education.
         // In real production environment, this has to be secure for SQL injection!
         connection.query("INSERT INTO `list3` (`id`, `title`, `quantity`, `unit`, `created_at`) VALUES (NULL, '" + title + "', '" + quantity + "', '" + unit + "', current_date());", function (error, results, fields) {
+        // SQL-Injection vermeiden:  
+        // connection.query("INSERT INTO 'list3' (`id`, `title`, `quantity`, `unit`, `created_at`) VALUES (NULL, '" + ? + "', '" + ? + "', '" + ? + "', current_date());") , [
+        // req.body.title,
+        // req.body.quantity,
+        // req.body.unit
+        //], function (error, results, fields) {    
             if (error) {
                 // we got an errror - inform the client
                 console.error(error); // <- log error in server
@@ -427,56 +452,56 @@ app.post('/database/list3', (req, res) => {
 
 // ###################### DATABASE PART (user) ######################
 // GET path for database
-app.get('/database/user', (req, res) => {
-    console.log("Request to load all entries from user");
-    // Prepare the get query
-    connection.query("SELECT * FROM `user`;", function (error, results, fields) {
-        if (error) {
-            // we got an errror - inform the client
-            console.error(error); // <- log error in server
-            res.status(500).json(error); // <- send to client
-        } else {
-            // we got no error - send it to the client
-            console.log('Success answer from DB: ', results); // <- log results in console
-            // INFO: Here could be some code to modify the result
-            res.status(200).json(results); // <- send it to client
-        }
-    });
-});
+// app.get('/database/user', (req, res) => {
+//     console.log("Request to load all entries from user");
+//     // Prepare the get query
+//     connection.query("SELECT * FROM `user`;", function (error, results, fields) {
+//         if (error) {
+//             // we got an errror - inform the client
+//             console.error(error); // <- log error in server
+//             res.status(500).json(error); // <- send to client
+//         } else {
+//             // we got no error - send it to the client
+//             console.log('Success answer from DB: ', results); // <- log results in console
+//             // INFO: Here could be some code to modify the result
+//             res.status(200).json(results); // <- send it to client
+//         }
+//     });
+// });
 
-// POST path for database
-app.post('/database/user', (req, res) => {
-    // This will add a new row. So we're getting a JSON from the webbrowser which needs to be checked for correctness and later
-    // it will be added to the database with a query.
-    if (typeof req.body !== "undefined" && typeof req.body.username !== "undefined" && typeof req.body.password !== "undefined") {
-        // The content looks good, so move on
-        // Get the content to local variables:
-        var username = req.body.username;
-        var password = req.body.password;
-        console.log("Client send database insert request with `username`: " + username + " ; password: " + password); // <- log to server
+// // POST path for database
+// app.post('/database/user', (req, res) => {
+//     // This will add a new row. So we're getting a JSON from the webbrowser which needs to be checked for correctness and later
+//     // it will be added to the database with a query.
+//     if (typeof req.body !== "undefined" && typeof req.body.username !== "undefined" && typeof req.body.password !== "undefined") {
+//         // The content looks good, so move on
+//         // Get the content to local variables:
+//         var username = req.body.username;
+//         var password = req.body.password;
+//         console.log("Client send database insert request with `username`: " + username + " ; password: " + password); // <- log to server
     
-        // Actual executing the query. Please keep in mind that this is for learning and education.
-        // In real production environment, this has to be secure for SQL injection!
-        connection.query("INSERT INTO `user` (`id`, `username`, `password`) VALUES (NULL, '" + username + "', '" + password + "');", function (error, results, fields) {
-            if (error) {
-                // we got an errror - inform the client
-                console.error(error); // <- log error in server
-                res.status(500).json(error); // <- send to client
-            } else {
-                // Everything is fine with the query
-                console.log('Success answer: ', results); // <- log results in console
-                // INFO: Here can be some checks of modification of the result
-                res.status(200).json(results); // <- send it to client
-            }
-        });
-    }
-    else {
-        // There is nobody with a title nor description
-        console.error("Client send no correct data!")
-        // Set HTTP Status -> 400 is client error -> and send message
-        res.status(400).json({ message: 'This function requries a body with "title", "quantity" and "unit"' });
-    }
-});
+//         // Actual executing the query. Please keep in mind that this is for learning and education.
+//         // In real production environment, this has to be secure for SQL injection!
+//         connection.query("INSERT INTO `user` (`id`, `username`, `password`) VALUES (NULL, '" + username + "', '" + password + "');", function (error, results, fields) {
+//             if (error) {
+//                 // we got an errror - inform the client
+//                 console.error(error); // <- log error in server
+//                 res.status(500).json(error); // <- send to client
+//             } else {
+//                 // Everything is fine with the query
+//                 console.log('Success answer: ', results); // <- log results in console
+//                 // INFO: Here can be some checks of modification of the result
+//                 res.status(200).json(results); // <- send it to client
+//             }
+//         });
+//     }
+//     else {
+//         // There is nobody with a title nor description
+//         console.error("Client send no correct data!")
+//         // Set HTTP Status -> 400 is client error -> and send message
+//         res.status(400).json({ message: 'This function requries a body with "title", "quantity" and "unit"' });
+//     }
+// });
 // ###################### DATABASE PART END (user) ######################
 
 
