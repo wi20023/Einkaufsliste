1aaeacf1133b0d2a6fee4df809faf3b8d394d3bd
wi20023 <89901288+wi20023@users.noreply.github.com>
2022-05-04 22:18:06 +0200

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -87,9 +87,11 @@ app.get('/', (req, res) => {
 //###################### Login (LoginDB; user) ######################
 app.use(session({
 	secret: '12345',
-	resave: true,
+	resave: false,
 	saveUninitialized: true,
-    Cookie: {}
+    Cookie: {
+        maxAge: 24 * 60 * 60 * 1000 // 24 hours
+    }
 }));
 
 // Poth path for database2 LoginDB, SELECT FROM user
@@ -111,7 +113,6 @@ app.post('/login', function(req, res) {
                 console.error(error); // <- log error in server
                 // If username does not exist redirect to login form
                 res.redirect('/static/login.html');
-                // res.send('Der eingegebene Benutzername ist nicht korrekt!');
             } else if (results.length > 0) {
             // get password for result
             var checkpass = results[0].password;
@@ -124,7 +125,7 @@ app.post('/login', function(req, res) {
                     req.session.username = username;
 
                     function SetName() {
-                        var name = req.sessio.username;
+                        var name = req.session.username;
                         '<%Session["name"] = "' + name + '"; %>';
                         alert('<%=Session["name"] %>');
                     }
@@ -142,11 +143,9 @@ app.post('/login', function(req, res) {
 		});
         }
 });
-
 // ###################### Login end (LoginDB; user)  ######################
 
 // ###################### Register (LoginDB; user)  ######################
-
 // POST path for database2 LoginDB, INSERT INTO user
 app.post('/register', (req, res) => {
     if (typeof req.body !== "undefined" && typeof req.body.username !== "undefined" && typeof req.body.password !== "undefined") {
@@ -178,7 +177,6 @@ app.post('/register', (req, res) => {
     }
 });
 // ###################### Register (LoginDB; user) ######################
-
 
 // ###################### DATABASE PART (EinkaufslisteDB; mainList) ######################
 // GET path for database EinkaufslisteDB, SELECT FROM mainList
@@ -254,10 +252,11 @@ app.post('/mainList', (req, res) => {
 }); 
 
 // POST path for database update
-app.post('/update/:id', (req, res) => {
+app.put('/update/', (req, res) => {
     if (typeof req.body !== "undefined" && typeof req.body.quantity !== "undefined" && typeof req.body.unit !== "undefined" && typeof req.body.note !== "undefined") {
 
         // Get the content to local variables:
+        var id = req.body.id;
         var quantity = req.body.quantity;
         var unit = req.body.unit;
         var note = req.body.note;
@@ -267,7 +266,7 @@ app.post('/update/:id', (req, res) => {
             quantity, 
             unit, 
             note, 
-            req.params.id
+            id
             ], function (error, results, fields) {
             
             if (error) {
@@ -277,12 +276,11 @@ app.post('/update/:id', (req, res) => {
             } else {
                 // Everything is fine with the query
                 console.log('Success answer: ', results); // <- log results in console
-                // INFO: Here can be some checks of modification of the result
                 res.status(200).json(results); // <- send it to client
             }
             });
         } else {
-        // There is nobody with a title nor description
+        // There value of data is wrong
         console.error("Client send no correct data!")
         // Set HTTP Status -> 400 is client error -> and send message
         res.status(400).json({ message: 'This function requries a body with "title", "quantity" and "unit"' });
@@ -292,7 +290,7 @@ app.post('/update/:id', (req, res) => {
 
 
 // ###################### DATABASE PART (EinkaufslisteDB; Drogerieprodukte) ######################
-// GET path for database EinkaufslisteDB, SELECT FROM Drogerieprodukte
+// GET path for database EinkaufslisteDB, SELECT * FROM Drogerieprodukte
 app.get('/Drogerieprodukte', (req, res) => {
     console.log("Request to load all entries from mainList");
     // Prepare the get query
@@ -359,6 +357,41 @@ app.post('/Drogerieprodukte', (req, res) => {
             });
         } else {
         // There is nobody with a title nor description
+        console.error("Client send no correct data!")
+        // Set HTTP Status -> 400 is client error -> and send message
+        res.status(400).json({ message: 'This function requries a body with "title", "quantity" and "unit"' });
+    }
+}); 
+// PUT path for database update
+app.put('/update_Drogerieprodukte/', (req, res) => {
+    if (typeof req.body !== "undefined" && typeof req.body.quantity !== "undefined" && typeof req.body.unit !== "undefined" && typeof req.body.note !== "undefined") {
+
+        // Get the content to local variables:
+        var id = req.body.id;
+        var quantity = req.body.quantity;
+        var unit = req.body.unit;
+        var note = req.body.note;
+        console.log("Client send database update request with quantity: " + req.body.quantity + " ; unit: " + req.body.unit + " ; note: " + req.body.note); // <- log to server
+
+        connection.query("UPDATE `Drogerieprodukte` SET `quantity` = ?, `unit` = ?, `note` = ?, `created_at` = current_date() WHERE `Drogerieprodukte`.`id` = ?", [
+            quantity, 
+            unit, 
+            note, 
+            id
+            ], function (error, results, fields) {
+            
+            if (error) {
+                // we got an errror - inform the client
+                console.error(error); // <- log error in server
+                res.status(500).json(error); // <- send to client
+            } else {
+                // Everything is fine with the query
+                console.log('Success answer: ', results); // <- log results in console
+                res.status(200).json(results); // <- send it to client
+            }
+            });
+        } else {
+        // There value of data is wrong
         console.error("Client send no correct data!")
         // Set HTTP Status -> 400 is client error -> and send message
         res.status(400).json({ message: 'This function requries a body with "title", "quantity" and "unit"' });
@@ -433,7 +466,43 @@ app.post('/Haustierbedarf', (req, res) => {
             }
             });
         } else {
-        // There is nobody with a title nor description
+        // The value of send data is wrong
+        console.error("Client send no correct data!")
+        // Set HTTP Status -> 400 is client error -> and send message
+        res.status(400).json({ message: 'This function requries a body with "title", "quantity" and "unit"' });
+    }
+}); 
+
+// PUT path for database update
+app.put('/update_Haustierbedarf/', (req, res) => {
+    if (typeof req.body !== "undefined" && typeof req.body.quantity !== "undefined" && typeof req.body.unit !== "undefined" && typeof req.body.note !== "undefined") {
+
+        // Get the content to local variables:
+        var id = req.body.id;
+        var quantity = req.body.quantity;
+        var unit = req.body.unit;
+        var note = req.body.note;
+        console.log("Client send database update request with quantity: " + req.body.quantity + " ; unit: " + req.body.unit + " ; note: " + req.body.note); // <- log to server
+
+        connection.query("UPDATE `Haustierbedarf` SET `quantity` = ?, `unit` = ?, `note` = ?, `created_at` = current_date() WHERE `Haustierbedarf`.`id` = ?", [
+            quantity, 
+            unit, 
+            note, 
+            id
+            ], function (error, results, fields) {
+            
+            if (error) {
+                // we got an errror - inform the client
+                console.error(error); // <- log error in server
+                res.status(500).json(error); // <- send to client
+            } else {
+                // Everything is fine with the query
+                console.log('Success answer: ', results); // <- log results in console
+                res.status(200).json(results); // <- send it to client
+            }
+            });
+        } else {
+        // There value of data is wrong
         console.error("Client send no correct data!")
         // Set HTTP Status -> 400 is client error -> and send message
         res.status(400).json({ message: 'This function requries a body with "title", "quantity" and "unit"' });
